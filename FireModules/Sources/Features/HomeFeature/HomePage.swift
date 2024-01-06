c77201758272e8218b6b649370199d04ca2d90f9
Hien Tran <hien.trquang@gmail.com>
2024-01-06 11:38:13 +0700

    Correct directories in swiftgen.yml, and change the endpoint to the EC2 one

--- FireModules/Sources/Features/HomeFeature/HomePage.swift
+++ FireModules/Sources/Features/HomeFeature/HomePage.swift
@@ -11,7 +11,7 @@ public struct HomePage: View {
     public init() {}
 
     public var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!")
     }
 }
 
